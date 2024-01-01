a60d93ae621550358abc171ea56a0e9156fd831a
Soham Roy <59397848+sohambuilds@users.noreply.github.com>
2024-01-01 16:56:35 +0530

    sensor detection
    (problem in coord of sensor color start nd end. find out issuez)

--- car.js
+++ car.js
@@ -1,26 +1,74 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,CType,MaxSpeed=3){
         this.x = x;
         this.y = y
         this.width = width;
         this.height = height;
         this.speed = 0;
-        this.acceleration = 0.01;
-        this.MaxSpeed=3;
-        this.friction = 0.005;
+        this.acceleration = 0.05;
+        this.MaxSpeed=MaxSpeed;
+        this.friction = 0.01;
         this.angle=0
-
-        this.controls = new Controls()
+        this.damaged = false
+        
+        if(CType!=="DUMMY"){this.sensor = new Sensor(this)}
+        //this.sensor = new Sensor(this)
+        this.controls = new Controls(CType)
 
     }
 
-    update(){
+    update(roadBorders,traffic){
+
+        if(!this.damaged){
 
         this.#move()
+        this.polygon = this.#createPolygon()
+        this.damaged=this.#checkDamage(roadBorders,traffic);
+        
+        }
+        if(this.sensor){this.sensor.update(roadBorders,traffic);}
+
+       
     
 
     }
 
+    #checkDamage(roadBorders,traffic){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
+                return true
+            }
+        }
+        return false
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
+    }
     #move(){
         if(this.controls.forward){
             this.speed += this.acceleration
@@ -51,24 +99,21 @@ class Car{
         this.y-= Math.cos(this.angle)*this.speed
     }
 
- draw(ctx){ 
+ draw(ctx,color){ 
+
+    if(this.damaged){
+        ctx.fillStyle="gray"
+    }else{
+        ctx.fillStyle=color
+    }
     ctx.save()
-    ctx.translate(this.x,this.y)
-    ctx.rotate(-this.angle)
-    
-    ctx.beginPath();
-
-    ctx.rect(
-
-    -this.width/2,
-    -this.height/2,
-    this.width,
-    this.height
-    );
-
+    ctx.moveTo(this.polygon[0].x,this.polygon[0].y)
+    for(let i=1;i<this.polygon.length;i++){
+        ctx.lineTo(this.polygon[i].x,this.polygon[i].y)
+    }
     ctx.fill();
-
-    ctx.restore();
+    if(this.sensor){this.sensor.draw(ctx)}
+    
 }
 }
 
--- main.js
+++ main.js
@@ -4,21 +4,28 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d")
 const road = new Road(canvas.width/2,canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1),100,30,50)
-
+const car = new Car(road.getLaneCenter(1),100,30,50, "KEYS")
+const traffic = [ new Car(road.getLaneCenter(2),100,30,50,"DUMMY",2)]
 
 animate();
 
 function animate(){
-    car.update();
+    for(i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[])
+    }
+    car.update(road.borders,traffic);
 
     canvas.height=window.innerHeight
 
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.5);
+  
 
     road.draw(ctx)
-    car.draw(ctx)
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(ctx,'green');
+    }
+    car.draw(ctx,'yellow')
     
     ctx.restore();
     requestAnimationFrame(animate)
